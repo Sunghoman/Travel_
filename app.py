da425192be9401d37abf4dededf3606e517b16d5
kimsungho <ksh_1035@naver.com>
2022-08-30 18:14:47 +0900

    diq

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from http import client
+import imp
 from flask import *
 import requests
 from bs4 import BeautifulSoup
-app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient("mongodb+srv://sungho:1464@cluster0.xcve6.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://sungho:1464@cluster0.xcve6.mongodb.net/test")
 db = client.sungho
+
+app = Flask(__name__)
+
 
 @app.route("/")
 def home():
   return render_template("index.html")
+
+@app.route("/post", methods = ["GET", "POST"])
+def post():
+  if request.method == "GET":
+    post_list = list(db.gmo.find({}, {"_id": False}))
+    return jsonify({"posts": post_list})
+    
+  return render_template("post.html")
