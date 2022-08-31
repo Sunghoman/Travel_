3419db8fd07e045fe77d3d6048e6bed2dec9f2d1
kimsungho <ksh_1035@naver.com>
2022-08-31 15:46:54 +0900

    포스팅 구현중

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from http import client
-import imp
 from flask import *
 import requests
 from bs4 import BeautifulSoup
@@ -15,10 +14,35 @@ app = Flask(__name__)
 def home():
   return render_template("index.html")
 
-@app.route("/post", methods = ["GET", "POST"])
+@app.route("/post")
 def post():
-  if request.method == "GET":
-    post_list = list(db.gmo.find({}, {"_id": False}))
-    return jsonify({"posts": post_list})
-    
   return render_template("post.html")
+
+
+@app.route("/posts", methods = ["GET"])
+def posts_get():
+  post_list = list(db.gmo.find({}, {"_id": False}))
+  return jsonify({"posts": post_list})
+
+@app.route("/posts", methods = ["POST"])
+def posts_post():
+  rate_receive = request.form["rate_give"]
+  img_receive = request.form["img_give"]
+  title_receive = request.form["title_give"]
+  comment_receive = request.form["comment_give"]
+  username_receive = request.form["username_give"]
+
+  doc = {
+    "rate": rate_receive,
+    "img": img_receive,
+    "title": title_receive,
+    "comment": comment_receive,
+    "username": username_receive
+  }
+
+  db.gmo_gangneung_post.insert_one(doc)
+  return jsonify({"msg": "저장 완료"})
+  
+@app.route("/detail")
+def detail():
+  return render_template("상세페이지.html")
